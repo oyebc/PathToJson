de5ab8fed15a7184bf4a9d6509cf848e87bd4bc1
oyebc <samueloyebisi108@gmail.com>
2016-03-23 16:37:24 -0400

    New addNodeWithName method

--- src/main/java/ai/maven/labs/DirTreeNode.java
+++ src/main/java/ai/maven/labs/DirTreeNode.java
@@ -4,8 +4,10 @@ import ai.maven.util.JSONUtils;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,11 +23,13 @@ public class DirTreeNode implements Serializable {
     private String name;
     //the map of the node to node is an hack to help retrieve an existing node in O(1) time.
     //there is an interesting conversation around it here: http://stackoverflow.com/questions/7283338/getting-an-element-from-a-set
-    private Map<DirTreeNode, DirTreeNode> children;
+    //private Map<DirTreeNode, DirTreeNode> children;
 
+    private List <DirTreeNode> children;
     public DirTreeNode(String name) {
         this.name = name;
-        this.children = new HashMap<>();
+       // this.children = new HashMap<>();
+        this.children = new ArrayList<>();
     }
 
     public String getName() {
@@ -33,25 +37,44 @@ public class DirTreeNode implements Serializable {
     }
 
     public Collection<DirTreeNode> getChildren() {
-        return children.values();
+        //return children.values();
+        return this.children;
     }
 
     /**
      * This is the crux of this code. Add a new node to the tree or retrieve node head if existing.
-     */
-    public DirTreeNode addOrGetNodeWithName(String child){
-        DirTreeNode childNode = null;
-        DirTreeNode keyNode = new DirTreeNode(child);
+//     */
+//    public DirTreeNode addOrGetNodeWithName(String child){
+//        DirTreeNode childNode = null;
+//        DirTreeNode keyNode = new DirTreeNode(child);
+//
+//        if (this.children.containsKey(keyNode)){
+//           childNode = this.children.get(keyNode);
+//        } else {
+//            childNode = new DirTreeNode(child);
+//            this.children.put(keyNode, childNode);
+//        }
+//        return childNode;
+//    }
 
-        if (this.children.containsKey(keyNode)){
-           childNode = this.children.get(keyNode);
-        } else {
-            childNode = new DirTreeNode(child);
-            this.children.put(keyNode, childNode);
+    public DirTreeNode addOrGetNodeWithName(String child){
+    
+        DirTreeNode childNode=null;
+        for (DirTreeNode currentNode: this.children)
+        {
+            if(currentNode.name.equals(child))
+            {
+                childNode = currentNode;
+                break;
+            }
+        }
+        if(childNode==null)
+        {
+             childNode = new DirTreeNode(child);
+             this.children.add(childNode);
         }
         return childNode;
     }
-
     /** For the purpose of identifying directories that have already been found in a level,
      * Directories are equal if they have the same name. We naively ignore their content
      */
